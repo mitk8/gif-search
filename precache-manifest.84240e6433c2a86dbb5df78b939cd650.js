2efb602893aa2a6fd4d503c4150c9505e7528738
Андрей Митько <andrey@MacBook-Pro-5.local>
2021-01-08 15:00:22 +0300

    Updates

---
+++ precache-manifest.84240e6433c2a86dbb5df78b939cd650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7f066bcce59b7808ae7b199211bffc",
+    "url": "/gif-search/index.html"
+  },
+  {
+    "revision": "77a3d189a9ab7efafdb5",
+    "url": "/gif-search/static/css/main.7a0e6566.chunk.css"
+  },
+  {
+    "revision": "bc95db14c62bbac61c80",
+    "url": "/gif-search/static/js/2.c4ad9278.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gif-search/static/js/2.c4ad9278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a3d189a9ab7efafdb5",
+    "url": "/gif-search/static/js/main.346a05b7.chunk.js"
+  },
+  {
+    "revision": "64b78f528396f1316ec4",
+    "url": "/gif-search/static/js/runtime-main.bc74187c.js"
+  }
+]);
